c574d323165f1109686cf388c2ab296762dc8705
Mcgroupfilm <130358785+Mcgroupfilm@users.noreply.github.com>
2024-05-01 10:03:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -193,7 +193,7 @@ async def next_page(bot, query):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[{get_size(file.file_size)}]{file.file_name}", callback_data=f'files#{file.file_id}'
+                        text=f"📀[{get_size(file.file_size)}]📀{file.file_name}", callback_data=f'files#{file.file_id}'
                     ),
                 ]
                 for file in files
@@ -202,10 +202,10 @@ async def next_page(bot, query):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                        text=f"🦜{file.file_name}", callback_data=f'files#{file.file_id}'
                     ),
                     InlineKeyboardButton(
-                        text=f"{get_size(file.file_size)}",
+                        text=f"{get_size(file.file_size☘️)}",
                         callback_data=f'files_#{file.file_id}',
                     ),
                 ]
